12780b71dbf4cbe642b2f919b21fa0f6e3e1214b
hans-hc <hansencheng635@gmail.com>
2025-08-09 14:22:17 -0400

    Update groupanagrams.py

--- groupanagrams.py
+++ groupanagrams.py
@@ -1,9 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = {}
+        result = {} #mapping charCount to the list of Anagrams
 
         for s in strs:
-            count = [0] * 26
+            count = [0] * 26 #a...z
             for c in s:
                 count[ord(c) - ord("a")] += 1
             key = tuple(count)
